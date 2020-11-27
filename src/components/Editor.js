bf9aaed57e6e573a4acada5ea4316bd69831b863
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2020-11-27 00:50:29 -0500

    updates to editor that finalize sync between websocket and editor component to BE

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,6 +7,7 @@ import { w3cwebsocket as W3CWebSocket } from "websocket";
 const client = new W3CWebSocket('ws://127.0.0.1:8000/ws/test_username')
 
 class TextEditor extends React.Component {
+
   constructor(props) {
     super(props);
     const contentState = props.text != null ? ContentState.createFromText(props.text) : ContentState.createFromText("");
@@ -18,6 +19,7 @@ class TextEditor extends React.Component {
     };
     this._onEditorChange = this._onEditorChange.bind(this);
     this._onWebsocketRecv = this._onWebsocketRecv.bind(this);
+    this.send_to_websocket = this.send_to_websocket.bind(this); 
   }
 
   _onEditorChange(editorState, newText=null) {
@@ -35,20 +37,16 @@ class TextEditor extends React.Component {
   
   _onWebsocketRecv(editorState, newText=null) {
     const content = editorState.getCurrentContent();
-    if(newText == null ){
+    if(newText == null){
       console.log("New Text is null, getting from content")
       newText = content.getPlainText();
     }
     if(typeof newText === 'string' || newText instanceof String){
       this.componentRecievedNewText({"text": newText})
-      //this.setState({
-          //editorState,
-          //text: newText // update state with new text whenever editor changes
-      //});
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     client.onopen = () => {
       console.log('WebSocket Client Connected');
     };
@@ -64,21 +62,24 @@ class TextEditor extends React.Component {
   }
 
   componentRecievedNewText(nextProps) {
-    console.log("NEW TEXT... comparing")
     const { editorState, text} = this.state;
-    const nextText = nextProps.text;
-    if (text !== nextText && nextText.trim() !== "") {   // check that text has changed before updating the editor
-      console.log("Setting text as: " + nextText.slice(0,-1))
-      const selectionState = editorState.getSelection();            
-      const newContentState = ContentState.createFromText(nextProps.text.slice(0,-1));            
-      const newEditorState = EditorState.create({
-              currentContent: newContentState,
-              selection: selectionState  // make sure the new editor has the old editor's selection state
-          });           
-      this.setState({
-          ...nextProps,
-          editorState: newEditorState
-      });
+    let nextText = String(nextProps.text);
+    // check that text has changed before updating the editor
+    if (String(text) !== String(nextText) && nextText.trim() !== "\"\"") {   
+      console.log("Setting text as: " + nextText.slice(1,-1))
+      let selectionState = editorState.getSelection(); 
+      if((selectionState === null || selectionState === undefined) && selectionState.getHasFocus()){
+        const newContentState = ContentState.createFromText(nextProps.text.slice(1,-1));            
+        let newEditorState = EditorState.forceSelection({
+                currentContent: newContentState,
+                selection: selectionState
+        });
+        this.setState({
+            ...nextProps,
+            editorState: newEditorState
+        });
+      }
+      
     }
   }
 
